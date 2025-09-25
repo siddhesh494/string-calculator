37e92271b33e4549928e16172e693aee9d7abe84
Siddhesh Shinde <siddheshshinde@Siddheshs-MacBook-Air.local>
2025-09-25 08:27:13 +0530

    returns number itself when only one number is provided

--- calculator.test.js
+++ calculator.test.js
@@ -4,4 +4,7 @@ test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
 
+test(`returns number itself when only one number is provided`, () => {
+  expect(add("1")).toBe(1)
+})
 
