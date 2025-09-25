b5e2810409f00e07da5cdcd0d577fc1a139a644c
Siddhesh Shinde <siddheshshinde@Siddheshs-MacBook-Air.local>
2025-09-25 09:45:44 +0530

    handles newlines as delimiters along with commas

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
+function replaceWithComma(text) {
+  return text.replace(/\n/g, ',');
+}
+
 function add(numbers) {
   if (numbers === "") return 0;
-
+  
+  numbers = replaceWithComma(numbers)
   const numArray = numbers.split(",").map(Number);
   let sum = 0
   numArray.forEach((n) => sum += n)
