ecdf4896dff07bf42c507b27b87011047e225e7c
Siddhesh Shinde <siddheshshinde@Siddheshs-MacBook-Air.local>
2025-12-26 09:24:51 +0530

    supports custom delimiter defined with // at the start

--- calculator.js
+++ calculator.js
@@ -2,15 +2,21 @@ function add(numbers) {
   if (numbers === "") return 0;
 
   // return parseInt(numbers);
-  numbers = replaceWithComma(numbers)
-  const numArray = numbers.split(",").map(Number);
+  let delimiter = /,|\n/; // default delimiters
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2)); // extract custom delimiter
+    numbers = parts[1];
+  }
+
+  const numArray = numbers.split(delimiter).map(Number);
   let sum = 0
   numArray.forEach((n) => sum += n)
   return sum
 }
 
-function replaceWithComma(text) {
-  return text.replace(/\n/g, ',');
-}
+// function replaceWithComma(text) {
+//   return text.replace(/\n/g, ',');
+// }
 
 module.exports = { add };
