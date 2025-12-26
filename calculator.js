534b6786e51ea486e78d0f2a8432274a526c9173
Siddhesh Shinde <siddheshshinde@Siddheshs-MacBook-Air.local>
2025-12-26 21:13:06 +0530

    sums only numbers <= 1000 when mixed with large numbers

--- calculator.js
+++ calculator.js
@@ -10,12 +10,17 @@ function add(numbers) {
   }
 
   const numArray = numbers.split(delimiter).map(Number);
+  
+  
+  
   const negatives = numArray.filter(num => num < 0);
   if (negatives.length > 0) {
     throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
   }
   let sum = 0
-  numArray.forEach((n) => sum += n)
+  numArray.forEach((n) => {
+    if(n <= 1000) sum += n
+  })
   return sum
 }
 
