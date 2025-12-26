75ec770c660ecb0efc9714b566747691a5c9d9ee
Siddhesh Shinde <siddheshshinde@Siddheshs-MacBook-Air.local>
2025-12-26 08:35:40 +0530

    returns sum of two comma-separated numbers

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  
-  return parseInt(numbers);
+
+  // return parseInt(numbers);
+  const numArray = numbers.split(",").map(Number);
+  let sum = 0
+  numArray.forEach((n) => sum += n)
+  return sum
 }
 
 module.exports = { add };
